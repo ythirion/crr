7450214403d1d3b931a77ff848c2bbb97cd30726
Yoan Thirion <yoan.thirion@gmail.com>
2024-08-16 09:27:18 +0200

    add calisthenics

---
+++ clean-code/object-calisthenics/exercise/LordOfTheRings/Character.cs
@@ -0,0 +1,10 @@
+namespace LordOfTheRings
+{
+    public sealed class Character
+    {
+        public string N { get; set; }
+        public string R { get; set; }
+        public Weapon W { get; set; }
+        public string C { get; set; } = "Shire";
+    }
+}

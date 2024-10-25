5a3eb364c8f7a420bc621e6214c0bdd2c46e91a1
Yoan Thirion <yoan.thirion@gmail.com>
2024-10-25 18:18:24 +0200

    feat: rename to linQ

---
+++ linq/Workshop.Answers/02. Queries/Data/PetType.cs
@@ -0,0 +1,12 @@
+﻿namespace Workshop.Answers._02._Queries.Data
+{
+    public enum PetType
+    {
+        Cat,
+        Dog,
+        Hamster,
+        Turtle,
+        Bird,
+        Snake
+    }
+}

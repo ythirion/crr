bb83be3ae46c797fa4eb721c6ead34a1b895ce91
Yoan Thirion <yoan.thirion@gmail.com>
2024-10-25 18:15:33 +0200

    feat: move files in linQ

--- linq/linq-workshop/Workshop.Answers/02. Queries/Data/Pet.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Workshop.Answers._02._Queries.Data
-{
-    public readonly struct Pet(PetType type, string name, int age)
-    {
-        public PetType Type { get; } = type;
-        public string Name { get; } = name;
-        public int Age { get; } = age;
-    }
-}

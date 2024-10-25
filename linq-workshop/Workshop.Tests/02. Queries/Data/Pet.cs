8f07a868419fa74daafb45911c8a99d2cf97213e
Yoan Thirion <yoan.thirion@gmail.com>
2024-10-25 18:17:43 +0200

    feat: rename to linQ

--- linq-workshop/Workshop.Tests/02. Queries/Data/Pet.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Workshop.Tests._01._Fundamentals.Data
-{
-    public readonly struct Pet(PetType type, string name, int age)
-    {
-        public PetType Type { get; } = type;
-        public string Name { get; } = name;
-        public int Age { get; } = age;
-    }
-}
